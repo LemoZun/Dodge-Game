95b8e132f827c59fbd9c8a578b6e40347574b8e5
LemoZun <marrzun@gmail.com>
2024-08-31 10:37:20 +0900

    refactor : 플레이어 이동 Translate -> RigidBody 변경
    rigidBody.velocity로 물리력을 줘서 이동
    GetKey -> GetAxis 로 변경

--- DodgeGame/Assets/Scripts/DodgeGame/PlayerController.cs
+++ DodgeGame/Assets/Scripts/DodgeGame/PlayerController.cs
@@ -16,6 +16,14 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
+        float xMovement = Input.GetAxis("Horizontal");
+        float zMovement = Input.GetAxis("Vertical");
+
+        //RigidBody로 이동 
+        rigid.velocity = new Vector3(xMovement * moveSpeed, 0, zMovement * moveSpeed);
+
+        /* GetKey로 이동 구현
+         * 이것보단 입력매니저 사용이 더 좋다(이동으로 할당된 모든 키로 이동 가능하기 때문)
         if(Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
@@ -32,5 +40,9 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
         }
+        */
     }
 }
+
+// GetAxis : 아날로그식 범위값 (-1 ~ 1)
+// GetAxisRaw : 디지털식 특정 값 (-1,0,1)
